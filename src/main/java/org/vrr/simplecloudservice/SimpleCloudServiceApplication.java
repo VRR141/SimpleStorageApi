ddeb4a388278c457983f975ec1149d0da684d256
Vladislav <v.r.ryazantsev@gmail.com>
2023-04-22 02:24:03 +0300

    fx main

--- src/main/java/org/vrr/simplecloudservice/SimpleCloudServiceApplication.java
+++ src/main/java/org/vrr/simplecloudservice/SimpleCloudServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class SimpleCloudServiceApplication {
-    
+
     public static void main(String[] args) {
        SpringApplication.run(SimpleCloudServiceApplication.class, args);
     }
