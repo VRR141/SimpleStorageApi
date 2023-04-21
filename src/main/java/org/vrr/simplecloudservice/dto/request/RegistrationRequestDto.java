03ab961e0e4a21198dfe1a57d9a9ac066e6e047f
Vladislav <v.r.ryazantsev@gmail.com>
2023-04-22 02:36:12 +0300

    add validation

--- src/main/java/org/vrr/simplecloudservice/dto/request/RegistrationRequestDto.java
+++ src/main/java/org/vrr/simplecloudservice/dto/request/RegistrationRequestDto.java
@@ -1,11 +1,14 @@
 package org.vrr.simplecloudservice.dto.request;
 
-//TODO add validation
-
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.vrr.simplecloudservice.util.pattern.ValidationPattern;
 
 @Data
 @NoArgsConstructor
@@ -13,12 +16,20 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegistrationRequestDto {
 
+    @Pattern(regexp = ValidationPattern.EMAIL_PATTERN)
+    @NotNull
     private String email;
 
+    @NotNull
+    @NotEmpty
     private String password;
 
+    @NotNull
+    @NotEmpty
     private String firstName;
 
+    @NotNull
+    @NotEmpty
     private String lastName;
 
     private String middleName;
--- src/main/java/org/vrr/simplecloudservice/rest/RegistrationController.java
+++ src/main/java/org/vrr/simplecloudservice/rest/RegistrationController.java
@@ -1,7 +1,9 @@
 package org.vrr.simplecloudservice.rest;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,13 @@ import org.vrr.simplecloudservice.facade.CloudServiceFacade;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class RegistrationController {
 
     private final CloudServiceFacade facade;
 
     @PostMapping("/register")
-    public ResponseEntity<Void> registerClient(@RequestBody RegistrationRequestDto dto){
+    public ResponseEntity<Void> registerClient(@Valid @RequestBody RegistrationRequestDto dto){
         facade.registerClient(dto);
         return ResponseEntity.ok().build();
     }
